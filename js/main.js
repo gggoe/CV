a71f087a9fda37a19a3816e9df9df8ba0ddd5b57
gggoe <gggoe@qq.com>
2017-06-16 21:04:50 +0800

    增加audio开关

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var mySwiper = new Swiper('.swiper-container', {
     loop: true,
     // 如果需要分页器
     pagination: '.swiper-pagination',
-    onTransitionEnd:function(swiper){
+    onTransitionEnd: function (swiper) {
         var slides = swiper.slides,
             curIndex = swiper.activeIndex,
             total = slides.length,
@@ -27,9 +27,37 @@ var mySwiper = new Swiper('.swiper-container', {
             }
         });
     }
-
-
 });
+
+/* audio */
+~function () {
+    var audioBtn = document.getElementById('music');
+    var music = document.getElementById('music-audio');
+    var musicBg = document.getElementsByClassName('control')[0];
+    var pauseBtn = document.getElementsByClassName('control-after')[0];
+    console.log(audioBtn, music)
+    var flag = true;
+    audioBtn.onclick = function () {
+        console.log(111)
+
+        flag = !flag;
+        if (flag) {
+            music.play();
+            musicBg.style.background="url('./images/public/music_1.gif') transparent no-repeat center center";
+            musicBg.style.backgroundSize="contain";
+            pauseBtn.style.animationPlayState='running';
+        } else{
+            music.pause();
+            musicBg.style.background='none';
+            pauseBtn.style.animationPlayState='paused';
+
+        }
+
+        console.log(flag);
+    }
+
+}();
+
 
 
 
